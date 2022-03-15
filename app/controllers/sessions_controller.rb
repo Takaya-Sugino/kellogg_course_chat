699ceaee6c1db13a41f55ebdec590dc87a78b1f7
anonymous anonymous <takaya.sugino@kellogg.northwestern.edu>
2022-03-15 00:11:02 +0000

    Remove controller comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,28 +1,23 @@
 class SessionsController < ApplicationController
   before_action :set_session, only: %i[show edit update destroy]
 
-  # GET /sessions
   def index
     @q = Session.ransack(params[:q])
     @sessions = @q.result(distinct: true).includes(:course, :professor,
                                                    :posts, :bookmarks, :users).page(params[:page]).per(10)
   end
 
-  # GET /sessions/1
   def show
     @bookmark = Bookmark.new
     @post = Post.new
   end
 
-  # GET /sessions/new
   def new
     @session = Session.new
   end
 
-  # GET /sessions/1/edit
   def edit; end
 
-  # POST /sessions
   def create
     @session = Session.new(session_params)
 
@@ -38,7 +33,6 @@ class SessionsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /sessions/1
   def update
     if @session.update(session_params)
       redirect_to @session, notice: "Session was successfully updated."
@@ -47,7 +41,6 @@ class SessionsController < ApplicationController
     end
   end
 
-  # DELETE /sessions/1
   def destroy
     @session.destroy
     message = "Session was successfully deleted."
@@ -60,12 +53,10 @@ class SessionsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_session
     @session = Session.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def session_params
     params.require(:session).permit(:course_id, :professor_id)
   end
